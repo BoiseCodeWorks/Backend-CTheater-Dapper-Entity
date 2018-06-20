cbffbf92c67048e108612fe05942cfa965902e84
Darryl Kilzer <darrylkilzer@gmail.com>
2018-06-19 21:51:35 -0600

    dapper

---
+++ CTheater/Controllers/MoviesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CTheater.Models;
+using CTheater.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTheater.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Movies")]
+    public class MoviesController : Controller
+    {
+        private readonly TheaterRepository _repo;
+
+        public MoviesController(TheaterRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: api/Movies
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return _repo.GetAll();
+        }
+
+        // GET: api/Movies/5
+        [HttpGet("{id}", Name = "Get")]
+        public Movie Get(string id)
+        {
+            return _repo.GetOne(id);
+        }
+        
+        // POST: api/Movies
+        [HttpPost]
+        public Movie Post([FromBody]Movie movie)
+        {
+            return _repo.Add(movie);
+        }
+        
+        // PUT: api/Movies/5
+        [HttpPut("{id}")]
+        public Movie Put(string id, [FromBody]Movie movie)
+        {
+            return _repo.GetOneByIdAndUpdate(id, movie);
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public string Delete(string id)
+        {
+            return _repo.FindByIdAndRemove(id);
+        }
+    }
+}
---
+++ CTheater/Repositories/TheaterRepository.cs
@@ -0,0 +1,70 @@
+﻿using CTheater.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CTheater.Repositories
+{
+    public class TheaterRepository
+    {
+        private readonly IDbConnection _db;
+
+        public TheaterRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Movie> GetAll()
+        {
+            return _db.Query<Movie>("SELECT * from movies");
+        }
+
+        public Movie GetOne(string id)
+        {
+            return _db.QueryFirstOrDefault<Movie>($"SELECT * FROM movies WHERE id = @id", new { id });
+        }
+
+        public Movie Add(Movie movie)
+        {
+            string Id = Guid.NewGuid().ToString();
+            int id = _db.ExecuteScalar<int>(@"
+                INSERT INTO movies (Id, Title, Description)
+                VALUES(@Id, @Title, @Description);
+                SELECT LAST_INSERT_ID()",
+                new
+                {
+                    Id,
+                    movie.Title,
+                    movie.Description
+                });
+
+            return new Movie()
+            {
+                Id = Id,
+                Title = movie.Title,
+                Description = movie.Description
+            };
+        }
+
+        public Movie GetOneByIdAndUpdate(string id, Movie movie)
+        {
+            movie.Id = id;
+            return _db.QueryFirstOrDefault<Movie>($@"
+                UPDATE movies SET
+                Title = @Title,
+                Description = @Description
+                WHERE Id = @id;
+                SELECT * FROM movies WHERE Id = @id", movie);
+        }
+
+        public string FindByIdAndRemove(string id)
+        {
+            var success = _db.Execute(@"
+            DELETE FROM movies WHERE Id = @id", new { id });
+            return success > 0 ? "Successfully deleted!" : "Couldn't delete that";
+        }
+    }
+}
